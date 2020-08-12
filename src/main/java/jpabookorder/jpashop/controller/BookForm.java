327686199c242a4e3b14fbe8adedae0fabcb144e
hmg612 <hmgcjn@gmail.com>
2020-08-12 16:41:57 +0900

    item register & list view 8/12
    item register & list view 8/12

---
+++ src/main/java/jpabookorder/jpashop/controller/BookForm.java
@@ -0,0 +1,17 @@
+package jpabookorder.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+    private Long id; // 상품 수정을 위해 id값 필요
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+
+}
---
+++ src/main/java/jpabookorder/jpashop/controller/ItemController.java
@@ -0,0 +1,45 @@
+package jpabookorder.jpashop.controller;
+
+import jpabookorder.jpashop.domain.item.Book;
+import jpabookorder.jpashop.domain.item.Item;
+import jpabookorder.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());   // 실무에서 setter는 가급적 사용X
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
