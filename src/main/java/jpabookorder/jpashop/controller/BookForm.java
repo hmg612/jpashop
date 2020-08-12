0284dab648e67a33ffdd267d8f28045607636311
hmg612 <hmgcjn@gmail.com>
2020-08-12 17:02:51 +0900

    item update view 8/12
    item update view 8/12

--- src/main/java/jpabookorder/jpashop/controller/BookForm.java
+++ src/main/java/jpabookorder/jpashop/controller/BookForm.java
@@ -7,10 +7,11 @@ import lombok.Setter;
 public class BookForm {
 
     private Long id; // 상품 수정을 위해 id값 필요
+
     private String name;
     private int price;
     private int stockQuantity;
-
+    
     private String author;
     private String isbn;
 
