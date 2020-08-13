37e53de86bd39074e143bc284e8b7d6b256de6c7
hmg612 <hmgcjn@gmail.com>
2020-08-13 17:47:03 +0900

    dirty checking 8/13
    dirty checking 8/13

--- src/main/java/jpabookorder/jpashop/service/ItemService.java
+++ src/main/java/jpabookorder/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabookorder.jpashop.service;
 
+import jpabookorder.jpashop.domain.item.Book;
 import jpabookorder.jpashop.domain.item.Item;
 import jpabookorder.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    //변경 감지 기능(dirty checking)
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+//      findItem.change(name, price, stockQuantity); -> 추적의 용이를 위해 실제로는 setter 사용보다 해당 메소드를 사용하도록 해야함
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
